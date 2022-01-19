afe44eef639d3d69e1d73a6ca4190b07ae72330e
Michael Grumbine <mgrumbine3@gmail.com>
2022-01-18 20:58:27 -0800

    Step 2 of TDD - Made it work (still need to make it better)

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-// write your code here to make the tests pass
+var Library = function () {
+
+  var shelves = {};
+  
+  var addBook = function (book) {
+    if (!shelves[book.getAttribute('title')]) {
+      shelves[book.getAttribute('title')] = book;
+    }
+  };
+  
+  var checkOutBook = function (book) {
+    if (shelves[book.getAttribute('title')]) {
+      shelves[book.getAttribute('title')].setAttribute('checkedOut', true);
+    }
+  };
+
+  var returnBook = function (book) {
+    if (shelves[book.getAttribute('title')]) {
+      book.setAttribute('checkedOut', false);
+    }
+  };
+
+  return {
+    returnBook: returnBook,
+    addBook: addBook,
+    checkOutBook: checkOutBook
+  }
+};
+
+var Book = function (title, author) {
+
+  var bookObj = {
+    title: title,
+    author: author,
+    checkedOut: false
+  };
+
+  var getAttribute = function (attribute) {
+    return bookObj[attribute];
+  };
+
+  var setAttribute = function (attribute, value) {
+    if (bookObj[attribute] !== undefined) {
+      bookObj[attribute] = value;
+    } else {
+      return 'Not a valid book attribute!';
+    }
+  };
+
+  return {
+    getAttribute: getAttribute,
+    setAttribute: setAttribute
+  };
+};
